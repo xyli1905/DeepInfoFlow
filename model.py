a3394c6233d32538855ced07210f8a61fe1b6c32
023proz <023zhangjiaoshou@gmail.com>
2019-04-28 16:43:12 -0700

    modify model.py to be more flexible

--- model.py
+++ model.py
@@ -2,29 +2,44 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 
+
 class model(nn.Module):
     def __init__(self):
         super(model,self).__init__()
+        self.layer_dims = [12, 12, 10, 7, 5, 4, 3, 2, 2]
+        self.D = {}
+        self.A = {}
+        self.construct_model_by_name('tanh')
 
-        self.Dense1 = nn.Linear(12, 12)
-        self.Dense2 = nn.Linear(12, 10)
-        self.Dense3 = nn.Linear(10, 7)
-        self.Dense4 = nn.Linear(7, 5)
-        self.Dense5 = nn.Linear(5, 4)
-        self.Dense6 = nn.Linear(4, 3)
-        self.Dense7 = nn.Linear(3, 2)
-        self.Dense8 = nn.Linear(2, 2)
 
-    def forward(self,x):
-        x = self.Dense1(x)
-        x = self.Dense2(x)
-        x = self.Dense3(x)
-        x = self.Dense4(x)
-        x = self.Dense5(x)
-        x = self.Dense6(x)
-        x = self.Dense7(x)
-        x = self.Dense8(x)
+    def construct_model_by_name(self, name):
+        depth = len(self.layer_dims) - 1
+        numOfActiv = depth - 1
 
+        if name == 'tanh':
+            for i in range(depth):
+                self.Dense = nn.Linear(self.layer_dims[i], self.layer_dims[i + 1])
+                if numOfActiv > 0:
+                    self.Activ = nn.Tanh()
+                    numOfActiv -= 1
+                    self.A[i] = self.Activ
+                self.D[i] = self.Dense
+        elif name == 'relu':
+            for i in range(depth):
+                self.Dense = nn.Linear(self.layer_dims[i], self.layer_dims[i + 1])
+                if numOfActiv > 0:
+                    self.Activ = nn.ReLU()
+                    numOfActiv -= 1
+                self.D[i] = self.Dense
+ 
+
+    def forward(self, x):
+        for i in range(len(self.layer_dims) - 1):
+            dense = self.D[i]
+            x = dense(x)
+            if i < len(self.A):
+                activ = self.A[i]
+                x = activ(x) 
         return x
 
 def weights_init(m):
@@ -43,7 +58,7 @@ if __name__ == '__main__':
     dummy_x = torch.randn(5, 12) #feature
     dummy_y = torch.randint(0,1,(5,2)) #label
     result = model.forward(dummy_x) #inference
-    # print(result)
+    print(result)
 
     criterion = nn.CrossEntropyLoss()# loss
     #example backprop
