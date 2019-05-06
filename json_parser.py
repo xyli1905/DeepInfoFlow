5628d75f81c57ab908c4f2ca2c587341bb08dda3
023proz <023zhangjiaoshou@gmail.com>
2019-05-05 18:36:51 -0700

    update api for jsonparser

--- json_parser.py
+++ json_parser.py
@@ -3,19 +3,18 @@ import pprint
 import argparse
 
 class JsonParser(object):
-    def __init__(self, opt, dir):
-        self.opt = opt
-        self.dir = dir
+    def __init__(self):
+        pass
 
-    def dump_json(self):
-        json_dir = self.dir + "opt.json"
-        argparse_dict = vars(self.opt)
+    def dump_json(self, opt, path):
+        json_dir = path + "opt.json"
+        argparse_dict = vars(opt)
         with open(json_dir, 'w') as outfile:
             json.dump(argparse_dict, outfile)
         print ("configs have benn dumped into %s" % json_dir)
 
-    def read_json_as_argparse(self):
-        json_dir = self.dir + "opt.json"
+    def read_json_as_argparse(self, path):
+        json_dir = path + "opt.json"
         js = open(json_dir).read()
         data = json.loads(js)
         opt = argparse.Namespace()
