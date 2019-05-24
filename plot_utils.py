cf14d9f7296a0596ae374df1b79c1f92bb23601f
Xingyu Li <xyli1905@gmail.com>
2019-05-24 10:24:30 -0700

    fix param in plot_svd to draw svd_w by default

--- plot_utils.py
+++ plot_utils.py
@@ -185,7 +185,7 @@ class PlotFigure:
         ax.tick_params(labelsize = 13)
 
 
-    def plot_svd(self, Lepoch, svd, IS_WEIGHT = False):
+    def plot_svd(self, Lepoch, svd, IS_WEIGHT = True):
         '''
         plot the variation of singular value for the averaged weight of each layer with respect to epoch
 
