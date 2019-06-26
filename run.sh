2e8cd2ff1e138c5926b3a109576e68b784839e27
Xingyu Li <xyli1905@gmail.com>
2019-06-26 11:22:32 -0700

    update param in run.sh

--- run.sh
+++ run.sh
@@ -1,20 +1,32 @@
 #
 PYTHON="/Users/xyli1905/anaconda3/envs/deepinfo/bin/python"
+# PYTHON=echo | which python
 
 # ------------------------- results for threshold ------------------------ #
-range_slope="1.0"
-range_dispX="0.0"
+range_slope="0.5 1.0 2.0"
 
 for slope in $range_slope
 do
+
+if [ "$slope" = "0.5" ]
+then
+    range_dispX="-2.5 -1.5 -1.0 -0.5 0.0 0.5 1.5"
+elif [ "$slope" = "1.0" ]
+then
+    range_dispX="-1.5 -0.5 0.0 0.5 1.5"
+elif [ "$slope" = "2.0" ]
+then
+    range_dispX="-1.0 -0.25 0.0 0.25 0.5 0.75 1.5"
+fi
 
 for dispX in $range_dispX
 do
 
 echo "run for: slope = $slope, dispX = $dispX"
+
 $PYTHON IBnet.py \
 --experiment_name test-tanhx-slope1_0-dispX0_0 \
---max_epoch 100 \
+--max_epoch 8000 \
 --activation tanhx \
 --Vmax 1.0 \
 --Vmin 0.0 \
